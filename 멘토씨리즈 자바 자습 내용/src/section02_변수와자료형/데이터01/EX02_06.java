dfda46d975e1d964b776c32bd91820069e887d4c
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 17:57:25 +0900

    멘토씨리즈 자바 예제 2-6번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/데이터01/EX02_06.java
@@ -0,0 +1,22 @@
+package section02_변수와자료형.데이터01;
+
+public class EX02_06 {
+
+	public static void main(String[] args) {
+
+		//예제 2-6 소수점 출력하기
+		
+		//%.nf: 소수점 아래 n번째 자리까지 출력(반올림)
+		System.out.printf("%.1f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.2f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.3f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.4f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.5f", 1.1234567);
+
+	}
+
+}
