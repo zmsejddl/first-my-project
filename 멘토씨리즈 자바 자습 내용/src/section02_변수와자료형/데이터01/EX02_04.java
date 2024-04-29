0bea10c4f953e9af8d6ed90b19f20e1575c21455
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 17:50:33 +0900

    멘토씨리즈 자바 예제 2-4번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/데이터01/EX02_04.java
@@ -0,0 +1,13 @@
+package section02_변수와자료형.데이터01;
+
+public class EX02_04 {
+
+	public static void main(String[] args) {
+
+		//예제 2-4 서식 문자를 이용해 출력하기 2
+		
+		//%d은 정수(10진수), %f은 실수, %s은 문자열
+		System.out.printf("%d은 첫 번째, %f은 두 번째, %s은 세 번째", 1, 2.0, "셋");
+	}
+
+}
