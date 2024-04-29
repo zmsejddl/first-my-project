e2aa63ccc89de1dba44c053d66931af886ea3b75
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 18:28:42 +0900

    멘토씨리즈 자바 예제 2-12번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/자료형03/EX02_12.java
@@ -0,0 +1,21 @@
+package section02_변수와자료형.자료형03;
+
+public class EX02_12 {
+
+	public static void main(String[] args) {
+
+		//예제 2-12 정수형 2
+		
+		//정수형에는 10진수뿐만 아니라 2, 8, 16진수의 형태로도 데이터를 대입할 수 있다
+		byte num1 = 0110;
+		short num2 = 0107;
+		int num3 = 0x46;
+		long num4 = 69L;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+
+}
