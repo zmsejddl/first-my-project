045227caa98e268605f64b11f4d57b62dfcaff76
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 17:33:00 +0900

    멘토씨리즈 자바 예제 2-2번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/데이터01/EX02_02.java
@@ -0,0 +1,15 @@
+package section02_변수와자료형.데이터01;
+
+public class EX02_02 {
+
+	public static void main(String[] args) {
+
+		//예제 2-2 Println() 출력문
+		
+		//괄호() 안의 데이터를 콘솔창에 출력
+		System.out.println("Welcome.");
+		//"Welcome." 문자열 아래 "JAVA World" 문자열 출력
+		System.out.println("JAVA World");
+	}
+
+}
