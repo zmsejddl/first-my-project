0541715893c458530e136bc60207a81ae1ee95f3
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 18:00:25 +0900

    멘토씨리즈 자바 예제 2-7번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/변수02/EX02_07.java
@@ -0,0 +1,15 @@
+package section02_변수와자료형.변수02;
+
+public class EX02_07 {
+
+	public static void main(String[] args) {
+
+		//예제 2-7 변수에 데이터 입력하기
+		
+		int stuedntAge; //학생의 나이를 저장할 수 있는 변수 선언
+		stuedntAge = 20; //변수에 값을 대입(저장)
+		
+		System.out.println(stuedntAge);
+	}
+
+}
