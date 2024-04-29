12e250740432e220ad2c77e7097b5067f0fd10e8
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 17:54:24 +0900

    멘토씨리즈 자바 예제 2-5번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/데이터01/EX02_05.java
@@ -0,0 +1,22 @@
+package section02_변수와자료형.데이터01;
+
+public class EX02_05 {
+
+	public static void main(String[] args) {
+
+		//예제 2-5 오른쪽 정렬하여 출력하기
+		
+		//"%-nd"는 n칸 만큼 확보한 후 왼쪽으로 정렬하여 출력하라는 뜻입니다
+		//만일 자신의 수가 정렬하려는 공간의 크기(n)보다 크면 정렬이 무시됩니다
+		System.out.printf("%5d", 1);
+		System.out.println();
+		System.out.printf("%5d", 12);
+		System.out.println();
+		System.out.printf("%5d", 123);
+		System.out.println();
+		System.out.printf("%5d", 1234);
+		System.out.println();
+		System.out.printf("%5d", 12345);
+	}
+
+}
