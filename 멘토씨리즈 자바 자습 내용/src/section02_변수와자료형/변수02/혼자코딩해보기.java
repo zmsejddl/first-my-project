ed35e87c4d80720ef56d3e27dae023439dfe4e22
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 18:17:42 +0900

    멘토씨리즈 자바 '혼자코딩해보기' 문제입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/변수02/혼자코딩해보기.java
@@ -0,0 +1,14 @@
+package section02_변수와자료형.변수02;
+
+public class 혼자코딩해보기 {
+
+	public static void main(String[] args) {
+
+		//변수 이름을 나이와 관련된 이름으로 규칙에 따라 지어보고,
+		//본인의 나이를 대입하여 출력해 보세요
+		
+		int myAge = 24;
+		System.out.println(myAge);
+	}
+
+}
