ae9926463de798d5f4c192027bdf8267b6bb3780
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 18:06:11 +0900

    멘토씨리즈 자바 예제 2-8번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/변수02/EX02_08.java
@@ -0,0 +1,14 @@
+package section02_변수와자료형.변수02;
+
+public class EX02_08 {
+
+	public static void main(String[] args) {
+
+		//예제 2-8 변수 선언 및 초기화
+		
+		String myCity = "Seoul"; //도시를 저장할 수 있는 변수를 선언하고,
+								 //변수에 "Seoul"을 대입(저장)했습니다
+		System.out.println("I am from " + myCity);
+	}
+
+}
