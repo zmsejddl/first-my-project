55f08881b3163ab6d15b88e3b75586d4af225599
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 18:09:33 +0900

    멘토씨리즈 자바 예제 2-9번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/변수02/EX02_09.java
@@ -0,0 +1,16 @@
+package section02_변수와자료형.변수02;
+
+public class EX02_09 {
+
+	public static void main(String[] args) {
+
+		//예제 2-9 변수를 이용해 다른 변수에 복사하기
+		
+		int myAge = 20;
+		int yourAge = myAge;
+		
+		System.out.println(myAge);
+		System.out.println(yourAge);
+	}
+
+}
