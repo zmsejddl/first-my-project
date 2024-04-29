03691ff571dbeb9f2e58631ff5a4feeaae0285eb
Jung Young Hun <hujung0268@gamil.com>
2024-04-29 17:47:08 +0900

    멘토씨리즈 자바 예제 2-3번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/데이터01/EX02_03.java
@@ -0,0 +1,12 @@
+package section02_변수와자료형.데이터01;
+
+public class EX02_03 {
+
+	public static void main(String[] args) {
+
+		//예제 2-3 서식 문자를 이용해 출력하기 1
+		
+		System.out.printf("저는 대학교 %d학년에 재학중입니다!", 3);
+	}
+
+}
