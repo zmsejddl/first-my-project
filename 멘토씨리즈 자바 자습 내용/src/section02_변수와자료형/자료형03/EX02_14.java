d566dd5ad14869630e9c2d21f055436b32c0115e
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 17:02:21 +0900

    멘토씨리즈 자바 예제 2-14번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/자료형03/EX02_14.java
@@ -0,0 +1,16 @@
+package section02_변수와자료형.자료형03;
+
+public class EX02_14 {
+
+	public static void main(String[] args) {
+
+		//예제 2-14 실수형2 <정밀도 확인하기>
+		
+		double pieDouble = 3.141592653589793;
+		float pieFloat = 3.141592653589793f;
+		
+		System.out.println("double: " + pieDouble);
+		System.out.println("float: " + pieFloat);
+	}
+
+}
