5f157dff049d15bdeb62703869a5868cb4080f21
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 18:22:41 +0900

    멘토씨리즈 자바 예제 3-1번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/단항_연산자03/EX03_01.java
@@ -0,0 +1,17 @@
+package section03_연산자.단항_연산자03;
+
+public class EX03_01 {
+
+	public static void main(String[] args) {
+
+		//예제 3-1 부호 연산자
+		
+		int x = 100;
+		int resultPlus = +x;	//+ 부호
+		int resultMinus = -x;	//- 부호
+		
+		System.out.println(resultPlus);
+		System.out.println(resultMinus);
+	}
+
+}
