9b0efa3b669585796ecde76f1f72bcc612734a62
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 17:17:56 +0900

    멘토씨리즈 자바 예제 2-20번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/참조_자료형04/EX02_20.java
@@ -0,0 +1,16 @@
+package section02_변수와자료형.참조_자료형04;
+
+public class EX02_20 {
+
+	public static void main(String[] args) {
+
+		//예제 2-20 참조 자료형
+		
+		String hi = "안녕하세요,";
+		String niceToMeetYou = "만나서 반갑습니다";
+		
+		System.out.println(hi);
+		System.out.println(niceToMeetYou);
+	}
+
+}
