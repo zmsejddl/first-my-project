5453d9fe9ecfb6b7877af6205992882b137881cb
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 17:13:27 +0900

    멘토씨리즈 자바 예제 2-18번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/자료형03/EX02_18.java
@@ -0,0 +1,16 @@
+package section02_변수와자료형.자료형03;
+
+public class EX02_18 {
+
+	public static void main(String[] args) {
+
+		//예제 2-18 논리형1
+		
+		boolean isStudent = true;
+		System.out.println(isStudent); //출력 결과: true
+		
+		//자바 언어는 대소문자를 구별한다. 
+		//true와 True는 다르게 취급하므로 True를 대입할 경우 오류가 발생한다
+	}
+
+}
