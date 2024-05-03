b817cdb0656fd3369f155314d8bebd13cce81628
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 17:11:00 +0900

    멘토씨리즈 자바 예제 2-17번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/자료형03/EX02_17.java
@@ -0,0 +1,18 @@
+package section02_변수와자료형.자료형03;
+
+public class EX02_17 {
+
+	public static void main(String[] args) {
+
+		//예제 2-17 문자형3
+		
+		int alphabetA = 'A'; //'A'와 매핑된 유니코드(정수) 65가 저장됨
+		int alphabetB = 'B'; //'B'와 매핑된 유니코드(정수) 66이 저장됨
+		System.out.println(alphabetA);
+		System.out.println(alphabetB);
+		
+		char alphabetC = 67; //유니코드(정수) 67과 매핑된 'C'가 저장됨
+		System.out.println(alphabetC); 
+	}
+
+}
