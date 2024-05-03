d42df5c4fa254390a839116f6ab8bc83e96581dc
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 18:10:17 +0900

    멘토씨리즈 자바 응용문제 2-4번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/응용문제/Practice_02_04.java
@@ -0,0 +1,14 @@
+package section02_변수와자료형.응용문제;
+
+public class Practice_02_04 {
+
+	public static void main(String[] args) {
+
+		int numI = 'A';
+		System.out.println(numI);
+		
+		char numC = 66;
+		System.out.println(numC);
+	}
+
+}
