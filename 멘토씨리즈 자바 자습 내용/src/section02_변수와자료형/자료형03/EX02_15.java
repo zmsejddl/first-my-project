ef2c45c3bd2ba2cc7230f805903957f9d74dafa0
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 17:06:02 +0900

    멘토씨리즈 자바 예제 2-15번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/자료형03/EX02_15.java
@@ -0,0 +1,20 @@
+package section02_변수와자료형.자료형03;
+
+public class EX02_15 {
+
+	public static void main(String[] args) {
+
+		//예제 2-15 문자형1
+		
+		char ga = '가';
+		char na = '나';
+		
+		char alphabetA = 'A';
+		char alphabetB = 'B';
+		
+		System.out.println(ga);
+		System.out.println(na);
+		System.out.println(alphabetA + ", " + alphabetB);
+	}
+
+}
