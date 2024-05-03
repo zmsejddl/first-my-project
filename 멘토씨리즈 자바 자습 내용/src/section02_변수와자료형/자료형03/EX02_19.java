5b6445ebe4e4f05f567caa64e33634922c59024b
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 17:15:23 +0900

    멘토씨리즈 자바 예제 2-19번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/자료형03/EX02_19.java
@@ -0,0 +1,18 @@
+package section02_변수와자료형.자료형03;
+
+public class EX02_19 {
+
+	public static void main(String[] args) {
+
+		//예제 2-19 논리형2
+		
+		boolean isStudent = true;
+		
+		if(isStudent) { //isStudent가 true이면,
+			System.out.println("저는 학생입니다");
+		} else { //isStudent가 false이면,
+			System.out.println("저는 학생이 아닙니다");
+		}
+	}
+
+}
