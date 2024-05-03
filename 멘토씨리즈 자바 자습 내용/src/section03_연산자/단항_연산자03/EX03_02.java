8ffc5f022fa6fba67dbcca5c1bd9fd4399f60552
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 18:24:48 +0900

    멘토씨리즈 자바 예제 3-2번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/단항_연산자03/EX03_02.java
@@ -0,0 +1,16 @@
+package section03_연산자.단항_연산자03;
+
+public class EX03_02 {
+
+	public static void main(String[] args) {
+
+		//예제 3-2 부호 연산자 <실수>
+		
+		double d = 1.11;
+		double result = -d;
+		
+		System.out.println(-d);
+		System.out.println(result);
+	}
+
+}
