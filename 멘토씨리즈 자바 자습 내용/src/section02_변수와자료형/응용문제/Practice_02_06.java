b5d66977db786724134996055201c2b977b29d1f
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 18:12:33 +0900

    멘토씨리즈 자바 응용문제 2-6번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/응용문제/Practice_02_06.java
@@ -0,0 +1,14 @@
+package section02_변수와자료형.응용문제;
+
+public class Practice_02_06 {
+
+	public static void main(String[] args) {
+
+		int n1 = 7;
+		System.out.println(n1);
+		
+		double n2 = (double) n1;
+		System.out.println(n2);
+	}
+
+}
