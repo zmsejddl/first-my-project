1a17fff582214f00fbc12d5a2cd6db77bc5b3bf0
Jung Young Hun <hujung0268@gamil.com>
2024-05-03 18:09:04 +0900

    멘토씨리즈 자바 응용문제 2-3번 입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section02_변수와자료형/응용문제/Practice_02_03.java
@@ -0,0 +1,12 @@
+package section02_변수와자료형.응용문제;
+
+public class Practice_02_03 {
+
+	public static void main(String[] args) {
+
+		String fruit = "Apple";
+		
+		System.out.println("I like " + fruit);
+	}
+
+}
