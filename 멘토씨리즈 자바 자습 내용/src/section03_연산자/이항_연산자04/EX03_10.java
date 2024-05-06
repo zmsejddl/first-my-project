693e8341251556bc64ac8ae08fa2bd1dc93e51e8
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:01:39 +0900

    멘토씨리즈 자바 예제 3-10

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/이항_연산자04/EX03_10.java
@@ -0,0 +1,14 @@
+package section03_연산자.이항_연산자04;
+
+public class EX03_10 {
+
+	public static void main(String[] args) {
+
+		//예제 3-10 산술 연산자2 <형 변환>
+		
+		double num1 = 1.2345;
+		int num2 = 6;
+		System.out.println(num1 + num2);	//num2를 double로 형 변환하여 연산
+	}
+
+}
