4ba37779d24f467747ee540e080e6cecbea1d4dc
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:16:35 +0900

    멘토씨리즈 자바 예제 3-16

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/삼항_연산자05/EX03_16.java
@@ -0,0 +1,13 @@
+package section03_연산자.삼항_연산자05;
+
+public class EX03_16 {
+
+	public static void main(String[] args) {
+
+		//예제 3-16 삼항 연산자1
+		
+		int num = (7 > 1) ? 1 : 2;
+		System.out.println(num);
+	}
+
+}
