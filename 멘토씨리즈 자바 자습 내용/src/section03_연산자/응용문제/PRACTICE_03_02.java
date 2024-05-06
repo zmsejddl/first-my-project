8fca2519d494e81a6146de2476c9c4f1b1818dfe
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:19:45 +0900

    멘토씨리즈 자바 응용문제 3-2

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/응용문제/PRACTICE_03_02.java
@@ -0,0 +1,15 @@
+package section03_연산자.응용문제;
+
+public class PRACTICE_03_02 {
+
+	public static void main(String[] args) {
+
+		int x = 613;
+		
+		System.out.println(++x);
+		System.out.println(++x);
+		System.out.println(x++);
+		System.out.println(--x);
+	}
+
+}
