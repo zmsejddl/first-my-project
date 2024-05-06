210d938062ee1e2923800831d007a288b0fcfad6
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:23:06 +0900

    멘토씨리즈 자바 응용문제 3-5

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/응용문제/PRACTICE_03_05.java
@@ -0,0 +1,13 @@
+package section03_연산자.응용문제;
+
+public class PRACTICE_03_05 {
+
+	public static void main(String[] args) {
+
+		String a = (7 > 9) ? "apple" : "banana";	//false -> banana 출력
+		a += " juice";
+		
+		System.out.println(a);
+	}
+
+}
