689a8948aa315ed94ab691719162ff78c679eeaa
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 14:53:42 +0900

    멘토씨리즈 자바 혼자코딩해보기

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/단항_연산자03/혼자코딩해보기.java
@@ -0,0 +1,14 @@
+package section03_연산자.단항_연산자03;
+
+public class 혼자코딩해보기 {
+
+	public static void main(String[] args) {
+
+		int num1 = 100;
+		
+		System.out.println(num1++);
+		System.out.println(++num1);
+		System.out.println(++num1);
+	}
+
+}
