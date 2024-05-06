ceedd8b65c6d9666f82df0d963cd1e1859edb999
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 14:54:58 +0900

    멘토씨리즈 자바 예제 3-8

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/단항_연산자03/EX03_08.java
@@ -0,0 +1,14 @@
+package section03_연산자.단항_연산자03;
+
+public class EX03_08 {
+
+	public static void main(String[] args) {
+
+		//예제 3-8 논리 부정 연산자
+		
+		boolean isHuman = false;
+		System.out.println(!isHuman);
+		System.out.println(isHuman);
+	}
+
+}
