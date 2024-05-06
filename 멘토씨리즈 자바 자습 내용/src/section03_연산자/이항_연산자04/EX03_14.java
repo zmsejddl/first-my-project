c93d1e1481ed8dfe587719415b4c1fe969231baa
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:09:46 +0900

    멘토씨리즈 자바 예제 3-14

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/이항_연산자04/EX03_14.java
@@ -0,0 +1,17 @@
+package section03_연산자.이항_연산자04;
+
+public class EX03_14 {
+
+	public static void main(String[] args) {
+
+		//예제 3-14 관계 연산자2
+		
+		int a = 10;
+		int b = 11;
+		
+		System.out.println(a <= b);		//코드1
+		System.out.println(a == b);		//코드2
+		System.out.println(a != --b);	//코드3
+	}
+
+}
