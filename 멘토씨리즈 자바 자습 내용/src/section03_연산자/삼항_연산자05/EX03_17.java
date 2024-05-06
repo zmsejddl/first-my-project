d67d2b91d733f10d6a43f1b9fba47d43afa23c0b
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:17:55 +0900

    멘토씨리즈 자바 예제 3-17

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/삼항_연산자05/EX03_17.java
@@ -0,0 +1,19 @@
+package section03_연산자.삼항_연산자05;
+
+public class EX03_17 {
+
+	public static void main(String[] args) {
+
+		//예제 3-17 삼항 연산자2
+		
+		int num = 0;
+		
+		if(7 > 1) {
+			num = 1;
+		} else {
+			num = 2;
+		}
+		System.out.println(num);
+	}
+
+}
