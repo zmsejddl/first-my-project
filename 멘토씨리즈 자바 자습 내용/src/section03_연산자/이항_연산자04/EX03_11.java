c331f1a21a0593b854e118f6967ca301d562f338
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:03:21 +0900

    멘토씨리즈 자바 예제 3-11

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/이항_연산자04/EX03_11.java
@@ -0,0 +1,15 @@
+package section03_연산자.이항_연산자04;
+
+public class EX03_11 {
+
+	public static void main(String[] args) {
+
+		//예제 3-11 산술 연산자3 <표현 범위>
+		
+		int result = 1000000 * 1000000;
+		System.out.println(result);
+		
+		//1000000000000이 아닌 엉뚱한 쓰레기 값
+	}
+
+}
