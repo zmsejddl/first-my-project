73b584f2058f01768f770fbe73a8b89c70072fab
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 14:42:12 +0900

    멘토씨리즈 자바 예제 3-3번입니다

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/단항_연산자03/EX03_03.java
@@ -0,0 +1,15 @@
+package section03_연산자.단항_연산자03;
+
+public class EX03_03 {
+
+	public static void main(String[] args) {
+
+		//예제 3-3 증감 연산자1
+		
+		int num = 10;
+		System.out.println(num);
+		num++;
+		System.out.println(num);
+	}
+
+}
