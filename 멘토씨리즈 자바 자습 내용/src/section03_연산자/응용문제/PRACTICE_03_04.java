63bef4d71b94c7d7ba4b0205317883e5ad620e3c
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 15:21:13 +0900

    멘토씨리즈 자바 응용문제 3-4

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/응용문제/PRACTICE_03_04.java
@@ -0,0 +1,13 @@
+package section03_연산자.응용문제;
+
+public class PRACTICE_03_04 {
+
+	public static void main(String[] args) {
+
+		double a = 300;
+		double b = 150;
+		
+		System.out.println(b/a);
+	}
+
+}
