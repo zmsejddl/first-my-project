99a73f3f5036ae892474b136e1d83584e782bfaf
Jung Young Hun <hujung0268@gamil.com>
2024-05-06 14:57:47 +0900

    멘토씨리즈 자바 예제 3-9

---
+++ 멘토씨리즈 자바 자습 내용/src/section03_연산자/이항_연산자04/EX03_09.java
@@ -0,0 +1,19 @@
+package section03_연산자.이항_연산자04;
+
+public class EX03_09 {
+
+	public static void main(String[] args) {
+
+		//예제 3-9 산술 연산자1
+		
+		int x = 100;
+		int y = 200;
+		
+		System.out.println(x + y);	//100+200
+		System.out.println(x - y);	//100-200
+		System.out.println(x * y);	//100*200
+		System.out.println(x / y);	//100/200 정수형의 계산으로 0.5가 아닌 0을 반환
+		System.out.println(x % y);	//100%200
+	}
+
+}
