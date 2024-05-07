440f4d8f8c6ac7f9e53c436375becd8e022ec515
Jung Young Hun <hujung0268@gamil.com>
2024-05-07 22:20:45 +0900

    예제 4-4

---
+++ 멘토씨리즈 자바 자습 내용/src/section04_제어문_1/if_else문04/EX04_04.java
@@ -0,0 +1,19 @@
+package section04_제어문_1.if_else문04;
+
+public class EX04_04 {
+
+	public static void main(String[] args) {
+
+		//예제 4-4 if-else 문2
+		
+		int a = 4;
+		int b = 10;
+		
+		if(a > b) {
+			System.out.println("a가 b보다 크다");
+		} else {
+			System.out.println("a가 b보다 작거나 같다");
+		}
+	}
+
+}
