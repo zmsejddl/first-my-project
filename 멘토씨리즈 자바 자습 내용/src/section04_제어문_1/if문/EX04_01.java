6040272b9f5766d82a38b42c55635717f456bb19
Jung Young Hun <hujung0268@gamil.com>
2024-05-07 22:08:53 +0900

    멘토씨리즈 자바 예제 4-1

---
+++ 멘토씨리즈 자바 자습 내용/src/section04_제어문_1/if문/EX04_01.java
@@ -0,0 +1,15 @@
+package section04_제어문_1.if문;
+
+public class EX04_01 {
+
+	public static void main(String[] args) {
+
+		//예제 4-1 if 문1
+		int result = 0;
+		if(3 > 2) {
+			result = 3;
+		}
+		System.out.println(result);
+	}
+
+}
