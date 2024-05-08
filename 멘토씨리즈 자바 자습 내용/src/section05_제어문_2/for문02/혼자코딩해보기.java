22b87f9a2a5feebeb037d7d797c442a4a402899c
Jung Young Hun <hujung0268@gamil.com>
2024-05-08 17:24:06 +0900

    혼바 코딩해 보기

---
+++ 멘토씨리즈 자바 자습 내용/src/section05_제어문_2/for문02/혼자코딩해보기.java
@@ -0,0 +1,16 @@
+package section05_제어문_2.for문02;
+
+public class 혼자코딩해보기 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		
+		for(int i = 10; i >= 1; i--) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
