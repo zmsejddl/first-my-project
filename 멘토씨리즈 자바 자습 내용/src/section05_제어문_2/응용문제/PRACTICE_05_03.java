deac6b06e277f80673367064196a1d630e6f3f35
Jung Young Hun <hujung0268@gamil.com>
2024-05-08 18:11:25 +0900

    응용문제 5-3

---
+++ 멘토씨리즈 자바 자습 내용/src/section05_제어문_2/응용문제/PRACTICE_05_03.java
@@ -0,0 +1,17 @@
+package section05_제어문_2.응용문제;
+
+public class PRACTICE_05_03 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			if(i % 2 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
