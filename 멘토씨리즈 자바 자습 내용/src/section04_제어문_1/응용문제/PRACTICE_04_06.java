d0baf2f9eda9223ac6a79c31b528880884f67bde
Jung Young Hun <hujung0268@gamil.com>
2024-05-08 17:12:12 +0900

    응용문제 4-6

---
+++ 멘토씨리즈 자바 자습 내용/src/section04_제어문_1/응용문제/PRACTICE_04_06.java
@@ -0,0 +1,15 @@
+package section04_제어문_1.응용문제;
+
+public class PRACTICE_04_06 {
+
+	public static void main(String[] args) {
+
+		int a = 25;
+		
+		switch(a/10) {
+		case 2 :
+			System.out.println("a는 20 이상의 숫자입니다");
+		}
+	}
+
+}
