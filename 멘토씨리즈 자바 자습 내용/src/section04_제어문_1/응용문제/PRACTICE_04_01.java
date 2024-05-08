69e658ff41884b16cc7c5e114fcb6c9821b7410d
Jung Young Hun <hujung0268@gamil.com>
2024-05-08 17:08:14 +0900

    응용문제 4-1

---
+++ 멘토씨리즈 자바 자습 내용/src/section04_제어문_1/응용문제/PRACTICE_04_01.java
@@ -0,0 +1,16 @@
+package section04_제어문_1.응용문제;
+
+public class PRACTICE_04_01 {
+
+	public static void main(String[] args) {
+
+		int num = 11;
+		int result = 1000;
+		
+		if(num > 10) {
+			result = 1100;
+		}
+		System.out.println(result);
+	}
+
+}
