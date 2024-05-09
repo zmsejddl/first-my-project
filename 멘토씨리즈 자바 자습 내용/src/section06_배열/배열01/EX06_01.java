dc5475de6b18da201d678f9933edf9ddc3a5cca6
Jung Young Hun <hujung0268@gamil.com>
2024-05-09 22:16:26 +0900

    예제 6-1

---
+++ 멘토씨리즈 자바 자습 내용/src/section06_배열/배열01/EX06_01.java
@@ -0,0 +1,15 @@
+package section06_배열.배열01;
+
+public class EX06_01 {
+
+	public static void main(String[] args) {
+
+		//예제 6-1 배열의 선언과 생성
+		
+		int[] arr = new int[4];
+		
+		//배열 변수(참조형 변수)를 출력
+		System.out.println(arr);
+	}
+
+}
