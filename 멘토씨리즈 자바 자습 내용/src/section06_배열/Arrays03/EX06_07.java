2b9b0339e28e661d2762d9adc713429660232965
Jung Young Hun <hujung0268@gamil.com>
2024-05-11 16:17:27 +0900

    예제 6-7

---
+++ 멘토씨리즈 자바 자습 내용/src/section06_배열/Arrays03/EX06_07.java
@@ -0,0 +1,21 @@
+package section06_배열.Arrays03;
+
+import java.util.Arrays;
+
+public class EX06_07 {
+
+	public static void main(String[] args) {
+		
+		//예제 6-7 Arrays.ToString()
+		
+		int[] arr = {1,6,2,3,10,7,4,5,8,9};
+		
+		System.out.println(Arrays.toString(arr));
+		
+		/*
+		 * 해설
+		 * 3: Arrays 기능을 사용하기 위해 선언합니다
+		 * 13: Arrays.toString(배열)은 배열을 하나의 문자열로 출력해 줍니다
+		 */
+	}
+}
