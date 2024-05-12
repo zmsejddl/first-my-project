998f4bbc8eb2c0eebf1b766c23a2879251917bdc
Jung Young Hun <hujung0268@gamil.com>
2024-05-12 16:19:49 +0900

    예제 7-8

---
+++ 멘토씨리즈 자바 자습 내용/src/section07_클래스/클래스의_구성03/EX07_08.java
@@ -0,0 +1,13 @@
+package section07_클래스.클래스의_구성03;
+
+public class EX07_08 {	//클래스 선언
+
+	//예제 7-8 메서드 사용하기
+	
+	int wheel = 4;	//클래스 필드 선언 및 초기화
+	
+	void ride() {	//메서드 선언
+		System.out.println("달립니다");
+		System.out.println("씽씽씽");
+	}
+}
