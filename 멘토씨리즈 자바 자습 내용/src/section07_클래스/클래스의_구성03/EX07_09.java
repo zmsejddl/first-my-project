a6542d37598c035bd32a722660e6433bc66f133b
Jung Young Hun <hujung0268@gamil.com>
2024-05-12 16:21:30 +0900

    예제 7-9

---
+++ 멘토씨리즈 자바 자습 내용/src/section07_클래스/클래스의_구성03/EX07_09.java
@@ -0,0 +1,15 @@
+package section07_클래스.클래스의_구성03;
+
+public class EX07_09 {	//클래스 선언
+
+	public static void main(String[] args) {
+
+		//예제 7-9 메서드 사용하기
+		
+		EX07_08 c = new EX07_08();	//클래스를 이용해서 객체 생성
+		c.ride();	//메서드 사용
+		c.ride();	//메서드 사용
+		c.ride();	//메서드 사용
+	}
+
+}
