80f58927523d47c7d2e132684cc7f402426d3fea
Jung Young Hun <hujung0268@gamil.com>
2024-05-12 16:08:29 +0900

    예제 7-4

---
+++ 멘토씨리즈 자바 자습 내용/src/section07_클래스/객체와_클래스/EX07_04.java
@@ -0,0 +1,16 @@
+package section07_클래스.객체와_클래스;
+
+class Cat {	//클래스 선언
+	
+}
+
+public class EX07_04 {	//클래스 선언
+
+	public static void main(String[] args) {
+
+		//예제 7-4 하나의 소스 파일에 하나의 클래스 선언
+		
+		Cat c = new Cat();	//클래스 사용 = 객체 사용
+	}
+
+}
