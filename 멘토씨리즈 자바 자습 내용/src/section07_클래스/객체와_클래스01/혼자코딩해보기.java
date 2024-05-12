e2c14e63ba9fffa03b14f3e1c3bda9828b8fffe6
Jung Young Hun <hujung0268@gamil.com>
2024-05-12 16:10:27 +0900

    혼자 코딩해 보기

---
+++ 멘토씨리즈 자바 자습 내용/src/section07_클래스/객체와_클래스01/혼자코딩해보기.java
@@ -0,0 +1,17 @@
+package section07_클래스.객체와_클래스01;
+
+public class 혼자코딩해보기 {
+
+	public static void main(String[] args) {
+
+		Tree tree = new Tree();
+	}
+
+}
+
+class Tree {
+	
+	public static void main(String[] args) {
+		
+	}
+}
