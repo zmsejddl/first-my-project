a3ca3f034b038abdfa2350714316d13e08282105
Jung Young Hun <hujung0268@gamil.com>
2024-05-12 16:06:42 +0900

    예제 7-3

---
+++ 멘토씨리즈 자바 자습 내용/src/section07_클래스/객체와_클래스/EX07_03.java
@@ -0,0 +1,11 @@
+package section07_클래스.객체와_클래스;
+
+public class EX07_03 {	//클래스 선언
+
+	public static void main(String[] args) {
+		
+		//예제 7-3 객체 생성
+		
+		EX07_02 e = new EX07_02();	//EX07_02 클래스를 이용해서 객체 생성
+	}
+}
