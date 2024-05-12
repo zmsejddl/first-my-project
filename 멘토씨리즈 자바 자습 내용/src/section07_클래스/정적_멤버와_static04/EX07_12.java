836bb6425d8b1083fbe589c5270ee9d29b207be6
Jung Young Hun <hujung0268@gamil.com>
2024-05-12 16:30:17 +0900

    예제 7-12

---
+++ 멘토씨리즈 자바 자습 내용/src/section07_클래스/정적_멤버와_static04/EX07_12.java
@@ -0,0 +1,12 @@
+package section07_클래스.정적_멤버와_static04;
+
+public class EX07_12 {	//클래스 선언
+
+	//예제 7-12 정적 멤버
+	
+	static String schoolName = "코리아 고등학교";	//정적 멤버 선언
+	
+	static void goToSchool() {	//정적 메서드 선언
+		System.out.println("학교에 갑니다");
+	}
+}
