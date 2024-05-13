9ebf27e86138ea8ff90d3dc0bab5a5f8f8c9409e
Jung Young Hun <hujung0268@gamil.com>
2024-05-13 17:12:13 +0900

    예제 8-7

---
+++ 멘토씨리즈 자바 자습 내용/src/section08_메서드/매개변수와_return02/EX08_07.java
@@ -0,0 +1,13 @@
+package section08_메서드.매개변수와_return02;
+
+public class EX08_07 {
+
+	public static void main(String[] args) {
+
+		//예제 8-7 매개변수가 있는 메서드 호출
+		
+		EX08_06 myBook = new EX08_06();	//객체 생성
+		myBook.count(3);	//myBook 인스턴스 count 메서드 호출
+	}
+
+}
