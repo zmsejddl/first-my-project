3d2f7c65fe90183e1ebadad8a939bb85b8a315a7
Jung Young Hun <hujung0268@gamil.com>
2024-05-13 17:10:44 +0900

    예제 8-6

---
+++ 멘토씨리즈 자바 자습 내용/src/section08_메서드/매개변수와_return02/EX08_06.java
@@ -0,0 +1,10 @@
+package section08_메서드.매개변수와_return02;
+
+public class EX08_06 {
+
+	//예제 8-6 매개변수가 있는 메서드 정의
+	
+	void count(int bookNum) {
+		System.out.println("책은 " + bookNum + "권 입니다.");
+	}
+}
