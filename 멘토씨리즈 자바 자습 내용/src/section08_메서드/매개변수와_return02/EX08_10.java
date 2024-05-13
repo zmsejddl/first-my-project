4f94bed1f8db8ba4e64c423b9b35b5a917688efb
Jung Young Hun <hujung0268@gamil.com>
2024-05-13 17:16:49 +0900

    예제 8-10

---
+++ 멘토씨리즈 자바 자습 내용/src/section08_메서드/매개변수와_return02/EX08_10.java
@@ -0,0 +1,14 @@
+package section08_메서드.매개변수와_return02;
+
+public class EX08_10 {
+
+	//예제 8-10 다른 자료형 2개를 매개변수로 받는 메서드 정의
+	
+	void introduce(String name, int age) {
+		System.out.println("제 이름은 " + name + "이고, 나이는 " + age + "세입니다");
+	}
+	
+	void hello() {
+		System.out.println("안녕하세요");
+	}
+}
