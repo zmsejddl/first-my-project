b76ef071032138800c8072975fb72d77d859f9e5
Jung Young Hun <hujung0268@gamil.com>
2024-05-13 17:13:24 +0900

    예제 8-8

---
+++ 멘토씨리즈 자바 자습 내용/src/section08_메서드/매개변수와_return02/EX08_08.java
@@ -0,0 +1,10 @@
+package section08_메서드.매개변수와_return02;
+
+public class EX08_08 {
+
+	//예제 8-8 매개변수가 2개 이상인 메서드 정의
+	
+	void sum(int num1, int num2) {
+		System.out.println("두 수의 합은 " + (num1+ num2) + "입니다");
+	}
+}
