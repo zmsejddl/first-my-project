41b1846b2e8f87dc1d3f9510f9909d893a3715bc
Jung Young Hun <hujung0268@gamil.com>
2024-05-13 17:04:57 +0900

    예제 8-3

---
+++ 멘토씨리즈 자바 자습 내용/src/section08_메서드/메서드01/EX08_03.java
@@ -0,0 +1,13 @@
+package section08_메서드.메서드01;
+
+public class EX08_03 {
+
+	public static void main(String[] args) {
+
+		//예제 8-3 다른 클래스에 있는 메서드 호출하기
+		
+		EX08_02 jogger = new EX08_02();	//객체 생성
+		jogger.run();	//jogger 인스턴스의 run() 메서드 호출
+	}
+
+}
