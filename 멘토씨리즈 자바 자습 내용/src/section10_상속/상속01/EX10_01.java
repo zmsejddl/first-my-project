68c67fabbfddea1c9a14961d1ebe1da3daef1ad4
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 15:39:36 +0900

    예제 10-1

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/상속01/EX10_01.java
@@ -0,0 +1,13 @@
+package section10_상속.상속01;
+
+public class EX10_01 {
+
+	//예제 10-1 상속
+	
+	String title;
+	int price;
+	
+	void info() {
+		System.out.println("책의 제목은 " + title + "이고, 가격은 " + price + "원입니다");
+	}
+}
