95645c83cb0503a7941e255ea6fe3de2707fa201
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 16:33:39 +0900

    예제 10-24

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/이차_상속05/EX10_24.java
@@ -0,0 +1,14 @@
+package section10_상속.이차_상속05;
+
+public class EX10_24 {
+
+	public static void main(String[] args) {
+		
+		//예제 10-24 N차 상속
+		
+		EX10_23 sb = new EX10_23(10);
+		sb.takePerson();
+		sb.ride();
+	}
+	
+}
