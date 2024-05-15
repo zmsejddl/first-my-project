28f2e6895064eaac4f33a03f6e23148ce1fbe735
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 16:24:43 +0900

    예제 10-20

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/접근_제한자04/access1/EX10_20.java
@@ -0,0 +1,12 @@
+package section10_상속.접근_제한자04.access1;
+
+public class EX10_20 {	//클래스 선언
+
+	public static void main(String[] args) {
+
+		//예제 10-20 접근 제한자 private
+		
+		EX10_19 a = new EX10_19(10);	//에러
+	}
+
+}
