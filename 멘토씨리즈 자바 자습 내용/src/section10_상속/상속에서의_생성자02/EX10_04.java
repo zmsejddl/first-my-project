c15b0a415f60f178c1213b906c59aff8a7320d26
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 15:46:10 +0900

    예제 10-4

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/상속에서의_생성자02/EX10_04.java
@@ -0,0 +1,14 @@
+package section10_상속.상속에서의_생성자02;
+
+public class EX10_04 {	//클래스 선언
+
+	//예제 10-4 상속에서 생성자
+	
+	String name;
+	int age;
+	
+	EX10_04(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
