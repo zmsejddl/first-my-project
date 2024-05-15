4c0a04abc7e3b46fd25a2e45794fa8fccef0e98f
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 15:55:10 +0900

    예제 10-8

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/오버라이딩03/EX10_08.java
@@ -0,0 +1,11 @@
+package section10_상속.오버라이딩03;
+
+public class EX10_08 extends EX10_07 {	//클래스 선언
+	
+	//예제 10-8 메서드 오버라이딩
+	
+	@Override
+	void powerOn() {
+		System.out.println("아이 러브 삼송");
+	}
+}
