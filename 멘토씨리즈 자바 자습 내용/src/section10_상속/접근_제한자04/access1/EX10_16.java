8809dae4028f14dbc0e9b7aa1e2967cf19678508
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 16:15:54 +0900

    예제 10-16

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/접근_제한자04/access1/EX10_16.java
@@ -0,0 +1,10 @@
+package section10_상속.접근_제한자04.access1;
+
+public class EX10_16 {	//클래스 선언
+
+	//예제 10-16 접근 제한자 protected
+	
+	protected void accessProtected() {
+		System.out.println("Protected 멤버에 접근하였습니다.");
+	}
+}
