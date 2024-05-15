d23dd04fda9646ecc850f2aea37ab241b79d03c0
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 15:44:10 +0900

    예제 10-3

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/상속01/EX10_03.java
@@ -0,0 +1,15 @@
+package section10_상속.상속01;
+
+public class EX10_03 {	//클래스 선언
+
+	public static void main(String[] args) {
+
+		//예제 10-3 상속
+		
+		EX10_02 comicBook = new EX10_02();
+		comicBook.title = "포켓몬";
+		comicBook.price = 4500;
+		comicBook.info();
+	}
+
+}
