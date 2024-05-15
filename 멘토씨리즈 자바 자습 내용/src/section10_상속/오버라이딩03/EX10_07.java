26b7a0a4e33c21452acd50bfa03e3255f0100093
Jung Young Hun <hujung0268@gamil.com>
2024-05-15 15:53:42 +0900

    예제 10-7

---
+++ 멘토씨리즈 자바 자습 내용/src/section10_상속/오버라이딩03/EX10_07.java
@@ -0,0 +1,14 @@
+package section10_상속.오버라이딩03;
+
+public class EX10_07 {	//클래스 선언
+
+	//예제 10-7 메서드 오버라이딩
+	
+	void powerOn() {
+		System.out.println("삑- 컴퓨터가 켜졌습니다.");
+	}
+	
+	void powerOff() {
+		System.out.println("컴퓨터가 종료됩니다.");
+	}
+}
