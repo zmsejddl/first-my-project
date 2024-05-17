385a1d19bc61b67188ccb3973c63217a6ec4792d
Jung Young Hun <hujung0268@gamil.com>
2024-05-17 17:35:47 +0900

    예제 11-9 부모 클래스 역할을 하는 클래스

---
+++ 멘토씨리즈 자바 자습 내용/src/section11_다형성과_타입_변환/다형성02/EX11_09.java
@@ -0,0 +1,14 @@
+package section11_다형성과_타입_변환.다형성02;
+
+public class EX11_09 {
+
+	//예제 11-9 부모 클래스 역할을 하는 클래스
+	
+	void powerOn() {
+		System.out.println("삑- 컴퓨터가 켜졌습니다");
+	}
+	
+	void powerOff() {
+		System.out.println("컴퓨터가 종료됩니다");
+	}
+}
