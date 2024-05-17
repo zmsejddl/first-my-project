0bc4c4dd9ff269e835c34e4cacc47e0cea6b1b57
Jung Young Hun <hujung0268@gamil.com>
2024-05-17 17:44:47 +0900

    예제 11-13 클래스 생성

---
+++ 멘토씨리즈 자바 자습 내용/src/section11_다형성과_타입_변환/다형성02/EX11_13.java
@@ -0,0 +1,12 @@
+package section11_다형성과_타입_변환.다형성02;
+
+public class EX11_13 extends EX11_09 {
+	
+	//예제 11-13 클래스 생성
+
+	@Override
+	void powerOn() {
+		super.powerOn();
+		System.out.println("사랑해요 LZ");
+	}
+}
