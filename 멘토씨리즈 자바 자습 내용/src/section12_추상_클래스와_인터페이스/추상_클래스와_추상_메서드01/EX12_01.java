e55c61ecc20544bbfe07c1a2a230abe73cb43f98
Jung Young Hun <hujung0268@gamil.com>
2024-05-17 18:39:08 +0900

    예제 12-1 추상 클래스

---
+++ 멘토씨리즈 자바 자습 내용/src/section12_추상_클래스와_인터페이스/추상_클래스와_추상_메서드01/EX12_01.java
@@ -0,0 +1,16 @@
+package section12_추상_클래스와_인터페이스.추상_클래스와_추상_메서드01;
+
+public abstract class EX12_01 {
+
+	//예제 12-1 추상 클래스
+	
+	String chef;
+	
+	public EX12_01(String chef) {
+		this.chef = chef;
+	}
+	
+	void info() {
+		System.out.println("이 레시피는 " + chef + " 셰프님의 레시피입니다");
+	}
+}
