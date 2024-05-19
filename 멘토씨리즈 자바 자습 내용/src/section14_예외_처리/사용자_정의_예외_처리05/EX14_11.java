9babfb9694b59f598bb32b5920e98cca4bf5d5c1
Jung Young Hun <hujung0268@gamil.com>
2024-05-19 14:09:21 +0900

    해설 추가

--- 멘토씨리즈 자바 자습 내용/src/section14_예외_처리/사용자_정의_예외_처리05/EX14_11.java
+++ 멘토씨리즈 자바 자습 내용/src/section14_예외_처리/사용자_정의_예외_처리05/EX14_11.java
@@ -13,4 +13,12 @@ public class EX14_11 extends Exception {
 	public String getMessage() {
 		return this.message;
 	}
+	
+	/*
+	 * 해설
+	 * 3: Exception 객체를 상속받습니다
+	 * 8~10: 객체를 선언할 때 예외 메시지를 입력받도록 합니다
+	 * 13~15: Exception이 지닌 getMessage() 메서드를 오버라이드하여 재정의합니다.
+	 * 입력받은 메시지를 리턴합니다
+	 */
 }
