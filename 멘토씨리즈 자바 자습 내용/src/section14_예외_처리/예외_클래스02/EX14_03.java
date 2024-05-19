e1bd0683745eb062c887e7f032cb5370f3410f2e
Jung Young Hun <hujung0268@gamil.com>
2024-05-19 13:28:05 +0900

    예제 14-3 ArrayIndexOutOfBoundException 예외

---
+++ 멘토씨리즈 자바 자습 내용/src/section14_예외_처리/예외_클래스02/EX14_03.java
@@ -0,0 +1,13 @@
+package section14_예외_처리.예외_클래스02;
+
+public class EX14_03 {
+
+	public static void main(String[] args) {
+
+		//예제 14-3 ArrayIndexOutOfBoundException 예외
+		
+		int[] arr = {1, 6, 7, 9, 10};
+		System.out.println(arr[6]);
+	}
+
+}
