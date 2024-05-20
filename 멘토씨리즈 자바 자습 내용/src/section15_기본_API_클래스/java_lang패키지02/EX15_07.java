aff667521f37ec3e16c65f72d8d519889fae7c93
Jung Young Hun <hujung0268@gamil.com>
2024-05-20 17:33:13 +0900

    예제 15-7 toString() 메서드를 재정의하여 객체의 정보를 반환

---
+++ 멘토씨리즈 자바 자습 내용/src/section15_기본_API_클래스/java_lang패키지02/EX15_07.java
@@ -0,0 +1,13 @@
+package section15_기본_API_클래스.java_lang패키지02;
+
+public class EX15_07 {
+
+	public static void main(String[] args) {
+
+		//예제 15-7 toString() 메서드를 재정의하여 객체의 정보를 반환
+		
+		EX15_06 car = new EX15_06("소나타", "현대");
+		System.out.println(car);
+	}
+
+}
