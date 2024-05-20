cf9d0c2419bc018b140f737f98d30656f89d0841
Jung Young Hun <hujung0268@gamil.com>
2024-05-20 18:29:54 +0900

    응용문제 15-3

---
+++ 멘토씨리즈 자바 자습 내용/src/section15_기본_API_클래스/응용문제/PRACTICE_15_03.java
@@ -0,0 +1,15 @@
+package section15_기본_API_클래스.응용문제;
+
+public class PRACTICE_15_03 {
+
+	public static void main(String[] args) {
+		
+		String str = "100";
+		int data1 = 200;
+		int result = 0;
+		
+		result = 100 + Integer.parseInt(str);
+		
+		System.out.println("숫자 합: " + result);
+	}
+}
